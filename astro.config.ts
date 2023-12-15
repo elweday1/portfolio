e6a404a24131ee6e58bccf0d7006381b79737873
elweday1 <mohammednh2864@gmail.com>
2023-12-15 19:21:08 +0200

    vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -5,10 +5,17 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+
   site: SITE.website,
   integrations: [
     tailwind({
