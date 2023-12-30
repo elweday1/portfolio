8cd8e097f51c87843daa5774bb389ca959c5592f
elweday1 <mohammednh2864@gmail.com>
2023-12-30 23:52:33 +0200

    ui tweaks

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,34 +5,23 @@ import type { CollectionEntry } from "astro:content";
 export interface Props {
   href?: string;
   frontmatter: CollectionEntry<"blog">["data"];
-  secHeading?: boolean;
 }
 
-export default function Card({ href, frontmatter, secHeading = true }: Props) {
+export default function Card({ href, frontmatter }: Props) {
   const { title, pubDatetime, description } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
-    className: "text-lg font-medium  hover:underline",
   };
 
   return (
-    <li className="my-6 flex flex-row p-3 bg-skin-fill rounded-lg min-h-40 ">
-      <div className="w-full">
-      <a
-        href={href}
-        className="py-1 inline-block text-xl scale-105 origin-top-left font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
-      >
-        {secHeading ? (
-          <h2 {...headerProps}>{title}</h2>
-        ) : (
-          <h3 {...headerProps}>{title}</h3>
-        )}
-      </a>
-      <Datetime datetime={pubDatetime} />
-      <p>{description}</p>
+    <li className="my-6 flex flex-row p-3 bg-skin-fill rounded-lg  h-full min-h-44">
+      <div className=" flex flex-col justify-between text-wrap w-full">
+        <a {...headerProps}   href={href}   className="py-1 inline-block text-xl scale-105 origin-top-left font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0   hover:underline" >{title}</a>
+        <p className="">{description}</p>
+        <Datetime className="scale-90 origin-bottom-left mt-auto  " datetime={pubDatetime} />
       </div>
-      <img  className="rounded-lg pl-3" src="https://picsum.photos/100" alt="" />
+      <img  className="rounded-lg ml-3 " src="https://picsum.photos/100" alt="" />
     </li>
   );
 }
