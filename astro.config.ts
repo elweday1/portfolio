d64f4602bca157a243fd95fa3c0528fc960f541e
elweday1 <mohammednh2864@gmail.com>
2024-01-03 15:44:51 +0200

    update

--- astro.config.ts
+++ astro.config.ts
@@ -5,9 +5,6 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-
-// import vercel
-import vercel from "@astrojs/vercel/serverless";
 import compress from "astro-compress";
 import preload from "astro-preload";
 
