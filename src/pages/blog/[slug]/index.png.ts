8b49497f2aad3d47db49ce9e57d0f2fde1e93484
elweday1 <mohammednh2864@gmail.com>
2024-01-12 20:57:26 +0200

    fixing bug, passing the whole collection instead of one element

--- src/pages/blog/[slug]/index.png.ts
+++ src/pages/blog/[slug]/index.png.ts
@@ -3,14 +3,14 @@ import { getCollection, type CollectionEntry } from "astro:content";
 import { generateOGImage } from "@utils/generateOgImages";
 
 export async function getStaticPaths() {
-  const projects = await getCollection("projects");
-  return projects.map(project => ({
-    params: { slug: project.slug },
-    props: projects,
+  const posts = await getCollection("blog");
+  return posts.map(post => ({
+    params: { slug: post.slug },
+    props: post,
   }));
 }
 
 export const GET: APIRoute = async ({ props }) =>
-  new Response(await generateOGImage(props as CollectionEntry<"projects">), {
+  new Response(await generateOGImage(props as CollectionEntry<"blog">), {
     headers: { "Content-Type": "image/png" },
   });
