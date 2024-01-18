1f21511605f6bbc3d6a61c2207ef382f2339870d
elweday1 <mohammednh2864@gmail.com>
2024-01-18 20:46:08 +0200

    refactor: over-engineering a simple feature

---
+++ src/utils/shows.ts
@@ -0,0 +1,47 @@
+  
+  export type Show = {
+    Title: string;
+    Year: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Awards: string;
+    Poster: string;
+    Ratings: { Source: string; Value: string }[];
+    Metascore: string;
+    imdbRating: string;
+    imdbVotes: string;
+    imdbID: string;
+    Type: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: string;
+  };
+  
+
+
+  const omdbHandler = (omdbApiKey: string) => {
+    return async (showName: string) => {
+        const res = await fetch(
+          `https://www.omdbapi.com/?t=${showName}&apikey=${omdbApiKey}`, // Free API key no worries
+          { cache: "force-cache" }
+        );
+        const data = await res.json();
+        return data as Show;
+      };
+
+} 
+
+export default async (apikey: string, showsList: string[]) =>  {
+    const getShow = omdbHandler(apikey);
+    return await Promise.all(showsList.map(async showName => await getShow(showName)))
+};
