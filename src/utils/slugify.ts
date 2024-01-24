27439f7ffce49f1bd40a5f907ac701499c0101b3
elweday1 <mohammednh2864@gmail.com>
2024-01-24 21:38:59 +0200

    sluggify utility

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,13 +1,7 @@
 import { slug as slugger } from "github-slugger";
 import type { CollectionEntry } from "astro:content";
-
 export const slugifyStr = (str: string) => slugger(str);
-
-type collectionData = CollectionEntry<"blog">["data"] | CollectionEntry<"projects">["data"]
-
-const slugify = (data: collectionData) =>
-data.postSlug ? slugger(data.postSlug) : slugger(data.title);
-
+type EntryData = (CollectionEntry<"blog"> | CollectionEntry<"projects">)["data"]
+const slugify = (data: EntryData) => slugger(data.title);
 export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
-
 export default slugify;
