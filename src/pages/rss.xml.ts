431cc14141c34368cb96846cfef3facc67f77d79
elweday1 <mohammednh2864@gmail.com>
2024-01-24 17:40:24 +0200

    rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,15 +7,18 @@ import { SITE } from "@config";
 export async function GET() {
   const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
+  const projects = await getCollection("blog");
+  const items = [...sortedPosts, ...projects].map(({ data, collection }) => ({
+    link: `${collection == "blog" ? "blog": "projects"}/${slugify(data)}`,
+    title: data.title,
+    description: data.description,
+    pubDate: new Date(data.dateTime),
+  })) 
+  
   return rss({
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: sortedPosts.map(({ data }) => ({
-      link: `posts/${slugify(data)}`,
-      title: data.title,
-      description: data.description,
-      pubDate: new Date(data.dateTime),
-    })),
+    items: items,
   });
 }
