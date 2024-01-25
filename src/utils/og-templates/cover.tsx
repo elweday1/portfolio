9e8728359730e3b4e8eaf979e0c135b05b4401f9
elweday1 <mohammednh2864@gmail.com>
2024-01-25 02:33:21 +0200

    no post card while seaeching

--- src/utils/og-templates/cover.tsx
+++ src/utils/og-templates/cover.tsx
@@ -2,11 +2,9 @@ import { SITE } from "@config";
 import { type Collection } from "../../types";
 
 export default async (entry: Collection) => {
-  
-  let src = (entry.collection === "blog" ? entry.data.cover : entry.data.media[0]);
-  return (
+    return (
     <div style={{  color: "rgba(255,255,255)", fontFamily: "Nunito", width: "100%", height: "100%", position: "relative", display: "flex", backgroundColor: "black" }}>
-<img style={{ width: "50%", aspectRatio: "1/1", height: "100%", objectFit: "cover", transform: "scaleX(-1)"}} width={500} height={500} alt="dsds" src={SITE.website + "/assets/404.png"} /> 
+      <img style={{ width: "50%", aspectRatio: "1/1", height: "100%", objectFit: "cover", transform: "scaleX(-1)"}} width={500} height={500} alt="dsds" src={SITE.website + "/assets/404.png"} /> 
       <h1 style={{position: "absolute", top: "0", right: "50px", fontFamily: "Nunito", fontWeight: "bold"}} >{SITE.title + " | " + (entry.collection === "blog" ? "Article" : "Project")}</h1>
       <div style={{position: "absolute", display: "flex", flexDirection: "column", width: "50%", top: "50%", right: "50px", fontWeight: "bold", color: "rgba(255,255,255)" }} >
         <h2 style={{fontSize: "64px"}}>{entry.data.title}</h2>
