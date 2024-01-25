0ad3d5d6ac05704e121937067b77d7e210419ec8
elweday1 <mohammednh2864@gmail.com>
2024-01-25 06:12:58 +0200

    we ball

--- src/utils/getSorted.ts
+++ src/utils/getSorted.ts
@@ -2,25 +2,19 @@ import  { getCollection, type CollectionEntry  } from "astro:content";
 
 
 
-type Both = ["blog", "projects"] | ["projects", "blog"];
-type Collection =  ["blog"] | ["projects"] | Both;
 
-type FunctionReturn<T extends Collection> = T extends ["blog"] ? CollectionEntry<"blog"> :
- T extends ["projects"] ? CollectionEntry<"projects"> :
- T extends Both? CollectionEntry<"projects"> | CollectionEntry<"blog"> :never
-
-type x =  FunctionReturn<["projects", "blog"]>
-const getSorted = async (...collection: Collection) => {
-
-  const items = await Promise.all(collection.map(async collection => await getCollection(collection))).then(entries => entries.flat());
-  const sortedItems = items
+type Collection =  "blog" | "projects";
+const getSorted = async (...itemTypes: Collection[]) => {
+const items = await Promise.all(itemTypes.map(async itemType => await getCollection(itemType))).then(entries => entries.flat());
+const sortedItems = items
     .filter(({ data }) => !data.draft)
     .sort(
       (a, b) =>
         new Date(b.data.dateTime).getTime() -
         new Date(a.data.dateTime).getTime()
     );
-      return sortedItems;
+      type x = typeof itemTypes;
+      return sortedItems  // idk ;
 } ;
 
 export default getSorted;
