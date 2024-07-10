9e082ed3e55d62a4be7674659672691f86697668
elweday1 <mohammednh2864@gmail.com>
2024-07-11 00:34:39 +0300

    rollback, nice try

--- src/pages/resume.ts
+++ src/pages/resume.ts
@@ -1,14 +1,24 @@
 export const prerender = false;
 import type { APIRoute } from "astro";
-import { RESUME, SITE } from "@config";
+import { RESUME } from "@config";
+import YAML from 'yaml'
 
+const { TELEGRAM_BOT_TOKEN, MY_CHAT_ID } = import.meta.env;
 
-export const GET: APIRoute = async ({ redirect, clientAddress }) => {
-  fetch(`${SITE.website}/notify-resume`, {
+async function sendToMe(clientAddress: string){
+const response = await fetch(`http://ip-api.com/json/${clientAddress}`);
+  const data = (await response.json()) as IpData;
+  const msg = encodeURIComponent(YAML.stringify(data));
+  const endPoint = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${MY_CHAT_ID}&text=${msg}`;
+  
+  await fetch(endPoint, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({clientAddress})
-  })
+  });
+}
+
+export const GET: APIRoute = async ({ redirect, clientAddress }) => {
+  await sendToMe(clientAddress); 
   return redirect(RESUME.normal);
 };
 
